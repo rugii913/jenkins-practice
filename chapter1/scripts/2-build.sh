77b8d9534206689a0f7b69e2ec575c6bcc27b87b
rugii913 <rugii913@gmail.com>
2024-12-11 22:17:15 +0900

    2-build.sh 경로 조정
    2-build.sh 경로 조정

--- chapter1/scripts/2-build.sh
+++ chapter1/scripts/2-build.sh
@@ -3,5 +3,5 @@
 
 #!/bin/bash
 mkdir deploy
-(cd projects/spring-app; gradle build) ## cf. ()는 subshell에서 명령을 실행하기 위한 구문 → cd 명령어와 같은 환경 변화가 원래의 shell에 영향을 주지 않음 ## cf. ;는 한 줄 안에서 여러 명령어를 연결하는 command separator
-cp projects/spring-app/build/libs/demo-0.0.1-SNAPSHOT.jar ./deploy/
+(cd ~/jenkins-practice/projects/spring-app; gradle build) ## cf. ()는 subshell에서 명령을 실행하기 위한 구문 → cd 명령어와 같은 환경 변화가 원래의 shell에 영향을 주지 않음 ## cf. ;는 한 줄 안에서 여러 명령어를 연결하는 command separator
+cp ~/jenkins-practice/projects/spring-app/build/libs/demo-0.0.1-SNAPSHOT.jar ./deploy/
